3893ad5ae1fd34c591dac6782185a73c7a2f4b92
Cole Robertson <cbjrobertson@gmail.com>
2024-05-05 09:22:29 -0400

    minor

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
      name='psy_llm_chat',  
-     version='0.1.3',
+     version='0.1.4',
      py_modules=['psy_llm_chat'] ,
      author="Cole Robertson",
      author_email="cbjrobertson@gmail.com",
      description="A Package for chatting with LLMs",
      long_description="A Package for chatting with LLMs",
-    long_description_content_type="text",
+    long_description_content_type="text/plain",
      url="https://github.com/cbjrobertson/llm_chat",
      packages=setuptools.find_packages(),
      classifiers=[
